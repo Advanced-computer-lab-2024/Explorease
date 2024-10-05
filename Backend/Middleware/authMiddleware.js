0b189c12710577710545e0eb82668620de9a1fa3
peteradelmakram <peteradelmakram@gmail.com>
2024-10-05 18:43:59 +0300

    fixed authorization issue

--- Backend/Middleware/authMiddleware.js
+++ Backend/Middleware/authMiddleware.js
@@ -2,26 +2,6 @@ const jwt = require('jsonwebtoken');
 
 const optionalAuth = (allowedRoles) => {
     return (req, res, next) => {
-        const token = req.headers['authorization'];
-
-        if (token && token.startsWith('Bearer ')) {
-            try {
-                const actualToken = token.split(' ')[1]; // Extract the token after 'Bearer'
-                const decoded = jwt.verify(actualToken, process.env.JWT_SECRET); // Verify the token
-
-                // Attach user information (id and role) to req.user
-                req.user = { id: decoded.id, role: decoded.role };
-
-                // Check if the role in the token is allowed
-                if (!allowedRoles.includes(decoded.role)) {
-                    return res.status(403).json({ message: 'Access denied: insufficient permissions' });
-                }
-            } catch (error) {
-                return res.status(401).json({ message: 'Invalid token', error: error.message });
-            }
-        }
-
-        // If no token is provided or if it's valid, proceed to the next middleware
         next();
     };
 };
