8efb0aa962bf2e694765745902fed11a692e64dc
peteradelmakram <peteradelmakram@gmail.com>
2024-12-08 19:18:54 +0200

    fixed homepage carousel & historical place tourist

--- frontend/src/Components/Tourist-Components/TouristHomePage.js
+++ frontend/src/Components/Tourist-Components/TouristHomePage.js
@@ -78,9 +78,9 @@ const TouristHomePage = ({profile}) => {
     }, []);
 
     const responsive = {
-        superLargeDesktop: { breakpoint: { max: 4000, min: 1024 }, items: 5 },
-        LargeDesktop: { breakpoint: { max: 2000, min: 1024 }, items: 4},
-        desktop: { breakpoint: { max: 1024, min: 768 }, items: 3},
+        superLargeDesktop: { breakpoint: { max: 4000, min: 1024 }, items: 2},
+        LargeDesktop: { breakpoint: { max: 2000, min: 1024 }, items: 2},
+        desktop: { breakpoint: { max: 1024, min: 768 }, items: 2},
         tablet: { breakpoint: { max: 768, min: 464 }, items: 2 },
         mobile: { breakpoint: { max: 464, min: 0 }, items: 1 },
     };
@@ -102,14 +102,14 @@ const TouristHomePage = ({profile}) => {
                         transform: 'scale(1.05)',
                         boxShadow: '0 6px 15px rgba(0, 0, 0, 0.3)',
                     },
-                    width: 350, // Fixed width
+                    width: 400, // Fixed width
                     height: 300, // Fixed height
                 }}
             >
                 <CardMedia
                     component="img"
                     height="140"
-                    image={placeholderImage} // Check for empty or blank string
+                    image={activity.imageUrl} // Check for empty or blank string
                     alt="Activity Image"
                     sx={{
                         height: "70%",
@@ -142,7 +142,7 @@ const TouristHomePage = ({profile}) => {
                         transform: 'scale(1.05)',
                         boxShadow: '0 6px 15px rgba(0, 0, 0, 0.3)',
                     },
-                    width: 350, // Fixed width
+                    width: 400, // Fixed width
                     height: 300, // Fixed height
                 }}
             >
@@ -433,117 +433,12 @@ const TouristHomePage = ({profile}) => {
             </Dialog>
         );
             
-
-    // const renderItineraryCard = (itinerary) => (
-    //     <Card
-    //         key={itinerary._id}
-    //         sx={{
-    //             margin: '10px',
-    //             marginBottom : '25px',
-    //             borderRadius: '10px',
-    //             boxShadow: '0 4px 10px rgba(0, 0, 0, 0.2)',
-    //             transition: 'transform 0.3s ease, box-shadow 0.3s ease',
-    //             '&:hover': {
-    //                 transform: 'scale(1.05)',
-    //                 boxShadow: '0 6px 15px rgba(0, 0, 0, 0.3)',
-    //             },
-    //         }}
-    //     >
-    //         <CardMedia
-    //             component="img"
-    //             height="140"
-    //             image={itinerary.imageUrl || placeholderImage}
-    //             alt="Itinerary Image"
-    //         />
-    //         <CardContent>
-    //             <Typography variant="h6" sx={{ fontWeight: 'bold', color: '#111E56' }}>
-    //                 {itinerary.name || 'Untitled'}
-    //             </Typography>
-    //             <Typography variant="body2" color="text.secondary">
-    //               <span style={{marginLeft : '2em'}}><strong>Activities:</strong></span>   {itinerary.activities?.map((a) => a.name).join(', ') || 'N/A'}
-    //             </Typography>
-    
-    //             {/* Available Dates Section */}
-    //             <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px', marginTop: '10px' }}>
-    //                 <CalendarTodayIcon sx={{ color: '#111E56' }} />
-    //                 {itinerary.AvailableDates && itinerary.AvailableDates.length > 0 ? (
-    //                     <Box sx={{ display: 'flex', gap: '4px', flexWrap: 'wrap' }}>
-    //                         {itinerary.AvailableDates.map((date, index) => (
-    //                             <Tooltip key={index} title={`Available Date: ${new Date(date).toLocaleDateString()}`} arrow>
-    //                                 <Chip
-    //                                     label={new Date(date).toLocaleDateString()}
-    //                                     sx={{
-    //                                         backgroundColor: '#E0E7FF',
-    //                                         color: '#111E56',
-    //                                         fontSize: '12px',
-    //                                         fontWeight: 'bold',
-    //                                     }}
-    //                                 />
-    //                                 {/* Each date shown as a chip */}
-    //                             </Tooltip>
-    //                         ))}
-    //                     </Box>
-    //                 ) : (
-    //                     <Typography variant="body2" color="text.secondary">
-    //                         No available dates.
-    //                     </Typography>
-    //                 )}
-    //             </Box>
-    //         </CardContent>
-    //     </Card>
-    // );
-    
-    // const renderHistoricalPlaceCard = (place) => (
-    //     <Card
-    //         key={place._id}
-    //         sx={{
-    //             margin: '10px',
-    //             marginBottom : '25px',
-    //             borderRadius: '10px',
-    //             boxShadow: '0 4px 10px rgba(0, 0, 0, 0.2)',
-    //             transition: 'transform 0.3s ease, box-shadow 0.3s ease',
-    //             '&:hover': {
-    //                 transform: 'scale(1.05)',
-    //                 boxShadow: '0 6px 15px rgba(0, 0, 0, 0.3)',
-    //             },
-    //         }}
-    //     >
-    //         <CardMedia
-    //             component="img"
-    //             height="140"
-    //             image={place.imageUrl || placeholderImage}
-    //             alt="Historical Place Image"
-    //         />
-    //         <CardContent>
-    //             <Typography variant="h6" sx={{ fontWeight: 'bold', color: '#111E56' }}>
-    //                 {place.Name || 'Untitled'}
-    //             </Typography>
-    //             <Typography variant="body2" color="text.secondary">
-    //                 {place.Description || 'No description available.'}
-    //             </Typography>
-    //             <Typography variant="body2" color="text.secondary">
-    //                 Ticket Prices:
-    //                 <ul>
-    //                     <li>Foreigner: ${place.TicketPrices?.foreigner || 'N/A'}</li>
-    //                     <li>Native: ${place.TicketPrices?.native || 'N/A'}</li>
-    //                     <li>Student: ${place.TicketPrices?.student || 'N/A'}</li>
-    //                 </ul>
-    //             </Typography>
-    //             <Tooltip title="View on Map">
-    //                 <IconButton>
-    //                     <LocationOnIcon sx={{ color: '#111E56' }} />
-    //                 </IconButton>
-    //             </Tooltip>
-    //         </CardContent>
-    //     </Card>
-    // );
-
     return (
         <Box
         sx={{
             padding: 0,
             margin: 0,
-            
+            marginTop : 6,
             //background: 'radial-gradient(circle, #111E56 0%, #d4d4d4 50%, #ffffff 100%)',
             backgroundSize: 'cover', // Ensures the gradient stretches to fit
             backgroundRepeat: 'no-repeat', // Prevents repeating
